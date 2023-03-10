4aa3fc7f1d0d6588b5e851750365c51a737327bb
bebaocute1010 <xduc24122002@gmail.com>
2023-03-10 16:36:10 +0700

    test

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -11,17 +11,41 @@ class OrderService
 {
 
     public $orderRepo;
+    public $orderInstance;
     public $responseService;
-
+    public $calShipping;
+    const SHIPPING_FLAT_RATE = 0;
+    const SHIPPING_FREE_SHIPPING = 1;
+    const SHIPPING_LOCAL_PICKUP = 2;
     public function __construct()
     {
         $this->orderRepo = new OrderRepository();
         $this->responseService = new ResponseService();
+        $this->calShipping = self::SHIPPING_FLAT_RATE;
+        $this->totalPayment = 0;
     }
-
+    public function calculateShipping() {
+        if ($this->totalPayment > 2000) {
+            $this->calShipping = self::SHIPPING_FREE_SHIPPING;
+        } else {
+            $this->shippingFee = min(30, $this->totalPayment * .2);
+        }
+    }
     public function create($data)
     {
-        return $this->orderRepo->create($data);
+        $this->orderInstance = $this->orderRepo->create($data);
+        $product_quantities = $data['product_quantities'];
+        foreach ($data['product_ids'] as $product_id) {
+            $this->orderInstance->details()->create([
+                'product_id'=> $product_id,
+                'quantity'=>$product_quantities[$product_id]
+            ]);
+        }
+        foreach($this->orderInstance->details as $detail) {
+            $this->totalPayment += $detail->product->price * $detail->quantity;
+        }
+        $this->calculateShipping();
+        return $this->orderInstance;
     }
 
     public function delete($id)
